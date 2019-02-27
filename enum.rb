7685870b6066fe1deb30b4aac53da60e3975f126
Gerald Bauer <gerald.bauer@gmail.com>
2019-02-27 22:17:26 +0100

    Create enum.rb

---
+++ enum.rb
@@ -0,0 +1,158 @@
+# encoding: utf-8
+
+##################################
+## auto-create/builds enum class.
+##
+## Example:
+##   State = Enum.new( :fundraising, :expired_refund, :successful)
+##     auto-creates/builds:
+##
+##  class Enum
+##    def initialize( key, value )
+##      @key   = key
+##      @value = value
+##    end
+##  end
+##
+##  class State < Enum
+##
+##    FUNDRAISING    = new(:fundraising,    0)
+##    EXPIRED_REFUND = new(:expired_refund, 1)
+##    SUCCESSFUL     = new(:successful,     2)
+##
+##    def self.fundraising()    FUNDRAISING; end
+##    def self.expired_refund() EXPIRED_REFUND; end
+##    def self.successful()     SUCCESSFUL; end
+##
+##    def fundraising?()     self == FUNDRAISING; end
+##    def expired_refund?()  self == EXPIRED_REFUND; end
+##    def successful?()      self == SUCCESSFUL; end
+##  end
+##
+##  pp state = State.fundraising     #=> #<State @key=:fundraising, @value=0>
+##  pp state.fundraising?            #=> true
+##  pp state.expired_refund?         #=> false
+##  pp state.successful?             #=> false
+##  pp state = State.expired_refund  #=> #<State @key=:expired_refund, @value=1>
+##  pp state.fundraising?            #=> false
+##  pp state.expired_refund?         #=> true
+##  pp state.successful?             #=> false
+
+
+module Safe
+
+
+## base class for enum
+class Enum
+  ## return a new Enum read-only class
+
+  def initialize( key, value )
+    @key   = key
+    @value = value
+    self.freeze   ## make "immutable"
+    self
+  end
+
+  ## add read-only attribute readers
+  ##  just use attr_reader!!! - why? why not?
+  def key()   @key;   end
+  def value() @value; end
+
+  def self.value( index )
+    ## todo/fix: check for out-of-bound / unknown enum
+    ## puts "#{name}.value(#{index})"
+    ## pp keys
+    ## pp values
+    members[ index ]
+  end
+
+  def self.zero
+    members[0]
+  end
+
+  def self.key( key )
+    @hash ||= Hash[ keys.zip( members ) ].freeze
+    @hash[key]
+  end
+
+  def self.[]( key )   ## convenience alias for key
+    self.key( key )
+  end
+
+  def self.values
+    @values ||= (0...keys.size).to_a.freeze
+    @values
+  end
+
+  def self.convert( arg )
+    ## todo/check: support keys too - why? why not?
+    ## e.g. Color(0), Color(1)
+    ##      Color(:red), Color(:blue) - why? why not?
+    value( arg )
+  end
+
+
+
+  ###################
+  ##  meta-programming "macro" - build class (on the fly)
+  def self.build_class( class_name, *keys )
+
+    ## check if all keys are symbols and follow the ruby id(entifier) naming rules
+    keys.each do |key|
+      if key.is_a?( Symbol ) && key =~ /\A[a-z][a-zA-Z0-9_]*\z/
+      else
+        raise ArgumentError.new( "[Enum] arguments to Enum.new must be all symbols following the ruby id naming rules; >#{key}< failed" )
+      end
+    end
+
+    klass = Class.new( Enum )
+
+    ## add self.new too - note: call/forward to "old" orginal self.new of Event (base) class
+    klass.define_singleton_method( :new ) do |*args|
+      old_new( *args )
+    end
+
+    keys.each_with_index do |key,index|
+      klass.class_eval( <<RUBY )
+        #{key.upcase} = new( :#{key}, #{index} )
+
+        def #{key}?
+          self == #{key.upcase}
+        end
+
+        def self.#{key}
+          #{key.upcase}
+        end
+RUBY
+    end
+
+    klass.class_eval( <<RUBY )
+      def self.keys
+        @keys   ||= #{keys}.freeze
+      end
+
+      def self.members
+        @members ||= [#{keys.map {|key|key.upcase}.join(',')}].freeze
+      end
+RUBY
+
+    ## note: use Safe (module) and NOT Object for namespacing
+    ##   use include Enum to make all enums and convenience converters global
+
+    ## add global convenience converter function
+    ##  e.g. State(0) is same as State.convert(0)
+    Safe.class_eval( <<RUBY )
+      def #{class_name}( arg )
+         #{class_name}.convert( arg )
+      end
+RUBY
+
+    Safe.const_set( class_name, klass )   ## returns klass (plus sets global constant class name)
+  end
+
+  class << self
+    alias_method :old_new, :new       # note: store "old" orginal version of new
+    alias_method :new,     :build_class    # replace original version with create
+  end
+end  # class Enum
+end # module Safe
