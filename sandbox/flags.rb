e84fe42bb1212b198feed4dc0d7f2cd8d68cf71e
Gerald Bauer <gerald.bauer@gmail.com>
2019-03-26 22:18:06 +0100

    Update flags.rb

--- sandbox/flags.rb
+++ sandbox/flags.rb
@@ -4,24 +4,6 @@ require 'pp'
 
 class Flag
 
-  class FlagSet
-     attr_reader :value
-     def initialize( value )
-       @value = value
-       self.freeze   ## make "immutable"
-       self
-     end
-     def flag_class() Flag; end
-     def |( other )
-       if flag_class == other.class
-         self.class.new( @value | other.value )
-       else
-          raise TypeError.new( "[FlagSet] flagset >#{flag_class.name}< type expected; got >#{other.class.inspect}<" )
-       end
-     end
-  end # nested class FlagSet
-
-
   attr_reader :key
   attr_reader :value
 
@@ -33,25 +15,50 @@ class Flag
   end
 
 
-  def self._typecheck( other )
-    if self == other.class
-      other
+  def self._typecheck!( o )
+    if self == o.class
+      o
     else
-      raise TypeError.new( "[Flag] flag >#{name}< type expected; got >#{other.class.inspect}<" )
+      raise TypeError.new( "[Flag] flag >#{name}< type expected; got >#{o.class.inspect}<" )
     end
   end
 
-  def |( other )
-    self.class._typecheck( other )
-    self.class::FlagSet.new( @value | other.value )
+  def self._value!( o )
+    if o.is_a? Integer
+      o
+    else
+      _typecheck!( o )
+      o.value
+    end
+  end
+
+  def bitwise_or( other )   ## bitwise-or
+    ## note: returns "plain" integer
+    @value | self.class._value!( other )
+  end
+  alias_method :|, :bitwise_or
+
+  def bitwise_inverse()  ## bitwise-inverse
+    ## note: returns "plain" integer
+     ~@value
+  end
+  alias_method :~, :bitwise_inverse
+
+
+  def coerce( other )
+    puts "[Flag] coerce( self= >#{self.inspect}<, other= >#{other.inspect}< #{other.class.name} )"
+    if other.is_a?( Integer )
+      [other, @value]
+    else
+      raise TypeError.new( "[Flag] coerce - wrong type >#{other.inspect}< #{other.class.name} - Integer number expected" )
+    end
   end
 end  # class Flag
 
 
 
 class Flags
-  def self.newzero() new; end
-  def self.zero()    @zero ||= newzero.freeze end
+  def self.zero() @zero ||= new(0); end
 
   def zero?() @value == 0; end
 
@@ -63,50 +70,93 @@ class Flags
       @value = 0   ## same as new(0)
     elsif args.size == 1 && args[0].is_a?(Integer)
       @value = args[0]
-    elsif args.size == 1 && args[0].is_a?(self.class::Flag::FlagSet)
-      @value = args[0].value
     else
       ## assume flag object or symbols
       @value = 0
       args.each do |arg|
-        flag = _typecast( arg )
+        flag = _typecast_flag!( arg )
         @value |= flag.value
       end
     end
+    self.freeze  ## make "immutable" - should be a value object like an integer number!!!
     self # return self for chaining
   end
 
 
-  def _typecast( o )
-    if o.is_a? Symbol   ## auto-convert symbol to flag
-      o = self.class.key( o )
-    end
+  def _value_for_flag!( o )
+    self.class::Flag._value!( o )
+  end
 
-    self.class::Flag._typecheck( o )
+  def _typecheck_flag!( o )
+    self.class::Flag._typecheck!( o )
+  end
+
+  def _typecast_flag!( o )
+    if o.is_a? Symbol   ## auto-convert symbol to flag
+      o = self.class.key( o )  ## lookup symbol in "parent" flags class
+    end
+    _typecheck_flag!( o )
   end
 
 
-  def member?( other ) _member?(_typecast( other )); end
+  def member?( other ) _member?(_typecast_flag!( other )); end
   def _member?( other ) @value & other.value  != 0; end
 
-  def set( other )  _set(_typecast( other )); end
-  def _set( other )
-    @value |= other.value
-    self ## return self for chaining
+
+  def bitwise_or( other )
+    _unsafe_bitwise_or( _value_for_flag!( other ) )
+  end
+  alias_method :|, :bitwise_or
+
+  def _unsafe_bitwise_or( other )   ## always assumes other is an integer
+    self.class.new( @value | other )
+  end
+
+  def set( other )  ## typesafe version of bitwise-or (|=) (no "plain" Integer allowed)
+    _unsafe_bitwise_or( _typecast_flag!( other ).value )
   end
   alias_method :flag, :set
 
-  def unset( other ) _unset(_typecast( other )); end
-  def _unset( other )
-    @value &= ~other.value
-    self ## return self for chaining
+
+  def bitwise_and( other )
+    _unsafe_bitwise_and( _value_for_flag!( other ) )
+  end
+  alias_method :&, :bitwise_and
+
+  def _unsafe_bitwise_and( other )
+    self.class.new( @value & other )
+  end
+
+
+  def unset( other )  ## typesafe version of bitwise-and/bitwise-inverse (&=~) (no "plain" Integer allowed)
+    _unsafe_bitwise_and( ~_typecast_flag!( other ).value )
   end
   alias_method :unflag, :unset
 
-  def toggle( other ) _toggle(_typecast( other )); end
-  def _toggle( other )
-    @value ^= other.value
-    self ## return self for chaining
+
+  def bitwise_xor( other )
+    _unsafe_bitwise_xor( _value_for_flag!( other ))
+  end
+  alias_method :^, :bitwise_xor
+
+  def _unsafe_bitwise_xor( other )
+    self.class.new( @value ^ other )
+  end
+
+  def toggle( other ) ## typesafe version of bitwise-xor (^=) (no "plain" Integer allowed)
+    _unsafe_bitwise_xor( _typecast_flag!( other ).value )
+  end
+
+
+
+  def self.keys()
+    # note: does NOT include none - why? why not?
+    @keys ||= members.map { |member| member.key }.freeze
+  end
+
+  def self.values()
+    # note: does NOT include none - why? why not?
+    @values ||= members.map { |member| member.value }.freeze
   end
 
 
@@ -125,8 +175,15 @@ class Flags
   end
 
 
-  ## add values, size|length too why? why not?
+  ### todo/fix:
+  ##  add ==/eql?
+  ##   for self AND flag AND integer
+  ##     always compare integer value
 
+
+  ## add size|length too why? why not?
+  ## add value() lookup?
+  ##   not for now - why? combined values are undefined!! what to return??
 
   ## add to_i, to_int - why? why not?
   ## def to_i()   @value; end
@@ -138,11 +195,8 @@ end  # class Flags
 
 class FileAttribs < Flags
 
-  class Flag < ::Flag
-    class FlagSet < ::Flag::FlagSet  ## add nested "typesafe" flagset class too
-      def flag_class() Flag; end
-    end
-  end
+  class Flag < ::Flag; end  # nested flag class for "typesafe" flags
+
   ## start with power (base) 0
   ## e.g. 2^0 or 1<<0 = 1 = 0001
   #       2^1 or 1<<1 = 2 = 0010
@@ -160,11 +214,6 @@ class FileAttribs < Flags
   def self.system()   SYSTEM;   end
   def self.archive()  ARCHIVE;  end
 
-  def self.keys()
-    # note: does NOT include none - why? why not?
-    @keys ||= [:readonly, :hidden, :system, :archive].freeze
-  end
-
   def self.members()
     # note: does NOT include none - why? why not?
     ## use flags - why? why not?
@@ -175,11 +224,6 @@ class FileAttribs < Flags
   def hidden?()   _member?( HIDDEN ); end
   def system?()   _member?( SYSTEM ); end
   def archive?()  _member?( ARCHIVE ); end
-
-  def readonly=(value)  value ? _set( READONLY ) : _unset( READONLY ); end
-  def hidden=(value)    value ? _set( HIDDEN )   : _unset( HIDDEN );   end
-  def system=(value)    value ? _set( SYSTEM )   : _unset( SYSTEM );   end
-  def archive=(value)   value ? _set( ARCHIVE )  : _unset( ARCHIVE );  end
 end  # class FileAttribs
 
 
@@ -188,70 +232,116 @@ module Kernel
 end # module Kernel
 
 
+pp FileAttribs.readonly
+pp FileAttribs.hidden
+pp FileAttribs.system
+pp FileAttribs.archive
 
 pp FileAttribs::READONLY
 pp FileAttribs::HIDDEN
 pp FileAttribs::SYSTEM
 pp FileAttribs::ARCHIVE
 
-pp FileAttribs.readonly
-pp FileAttribs.hidden
-pp FileAttribs.system
-pp FileAttribs.archive
+pp FileAttribs[:readonly]
+pp FileAttribs[:hidden]
+pp FileAttribs[:system]
+pp FileAttribs[:archive]
+
+
+pp FileAttribs.readonly.value
+pp FileAttribs.readonly.key
+pp FileAttribs.readonly.is_a? Flag
+pp FileAttribs.readonly.is_a? FileAttribs::Flag
+
 
 
 pp FileAttribs.new
 pp FileAttribs.new(0)
 pp FileAttribs.new( 1<<1 | 1<<3 )
+pp FileAttribs.new( FileAttribs.readonly, FileAttribs.hidden )
+pp FileAttribs.new( FileAttribs.readonly | FileAttribs.hidden )
+pp FileAttribs.new( FileAttribs.readonly | FileAttribs.hidden | FileAttribs.archive )
 pp FileAttribs.new( FileAttribs::READONLY | FileAttribs::HIDDEN )
 pp FileAttribs.new( FileAttribs::READONLY, FileAttribs::HIDDEN )
-pp FileAttribs.new( FileAttribs.readonly, FileAttribs.hidden )
-pp FileAttribs.new( FileAttribs.readonly | FileAttribs.hidden | FileAttribs.archive )
 pp FileAttribs.new( :readonly, :hidden )
 
-pp FileAttribs.newzero
 pp FileAttribs.zero
-
 pp FileAttribs(0)
 pp FileAttribs( :readonly )
 pp FileAttribs( :readonly, :hidden )
 
+pp FileAttribs( FileAttribs.readonly, FileAttribs.hidden )
+pp FileAttribs( FileAttribs.readonly | FileAttribs.hidden )
+pp FileAttribs( FileAttribs.readonly | FileAttribs.hidden | FileAttribs.archive )
 pp FileAttribs( FileAttribs::READONLY | FileAttribs::HIDDEN )
 pp FileAttribs( FileAttribs::READONLY, FileAttribs::HIDDEN )
-pp FileAttribs( FileAttribs.readonly, FileAttribs.hidden )
-pp FileAttribs( FileAttribs.readonly | FileAttribs.hidden | FileAttribs.archive )
 
 
 
 pp attribs = FileAttribs.new
+pp attribs.is_a? Flags
+pp attribs.is_a? FileAttribs
+
+
+puts "set (|=):"
+pp attribs.set( FileAttribs.readonly )   # note: set returns a new object
 pp attribs.set( FileAttribs::READONLY )
-pp attribs.set( FileAttribs::HIDDEN )
-pp attribs.unset( FileAttribs::SYSTEM )
-pp attribs.toggle( FileAttribs::SYSTEM )
-
-pp attribs.set( FileAttribs.readonly )
-pp attribs.set( FileAttribs.hidden )
-pp attribs.unset( FileAttribs.system )
-pp attribs.toggle( FileAttribs.system )
-
 pp attribs.set( :readonly )
+pp attribs |  FileAttribs.readonly       # same as FileAttribs#set
+pp attribs |= FileAttribs.readonly
+pp attribs |= FileAttribs::READONLY
+pp attribs |= FileAttribs[:readonly]
+
+pp attribs.set( FileAttribs.hidden )
+pp attribs.set( FileAttribs::HIDDEN )
 pp attribs.set( :hidden )
-pp attribs.unset( :system )
+pp attribs |  FileAttribs.hidden
+pp attribs |= FileAttribs.hidden
+pp attribs |= FileAttribs::HIDDEN
+
+
+puts "toggle (^=):"
+pp attribs
+pp attribs.toggle( FileAttribs.system )  # note: toggle returns a new object
+pp attribs.toggle( FileAttribs::SYSTEM )
 pp attribs.toggle( :system )
+pp attribs ^  FileAttribs.system        # same as FileAttribs#toggle
+pp attribs ^= FileAttribs.system
+pp attribs ^= FileAttribs.system
+pp attribs ^= FileAttribs.system
+pp attribs ^= FileAttribs::SYSTEM
+
+puts "unset (&=~):"
+pp attribs.unset( FileAttribs.system )   # note: unset returns a new object
+pp attribs.unset( FileAttribs::SYSTEM )
+pp attribs.unset( :system )
+pp attribs &= ~FileAttribs.system
+pp attribs &= ~FileAttribs::SYSTEM
+
 
 pp attribs.readonly?
 pp attribs.hidden?
 pp attribs.system?
 
-pp attribs.readonly = true
-pp attribs.hidden   = true
-pp attribs.system   = true
-
-pp attribs.readonly = false
-pp attribs.hidden   = false
-pp attribs.system   = false
 pp attribs.value
 
 
-pp FileAttribs.keys
 pp FileAttribs.members
+pp FileAttribs.keys
+pp FileAttribs.values
+
+attribs = FileAttribs( FileAttribs.readonly | FileAttribs.hidden )
+pp attribs
+pp attribs.readonly?
+pp attribs & FileAttribs.readonly
+pp attribs & FileAttribs.readonly == FileAttribs.readonly
+pp attribs & FileAttribs.readonly == 0
+pp attribs.hidden?
+pp attribs.system?
+
+
+pp attribs == FileAttribs.readonly
+pp FileAttribs.readonly == FileAttribs.readonly
+pp FileAttribs.readonly == FileAttribs::READONLY
+pp FileAttribs.readonly == FileAttribs.hidden
+pp FileAttribs.zero == 0
