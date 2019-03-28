795ed13701da8046156b7699c6909607d2d8bda6
Gerald Bauer <gerald.bauer@gmail.com>
2019-03-28 16:22:05 +0100

    Update flag.rb

--- lib/enums/flag.rb
+++ lib/enums/flag.rb
@@ -10,46 +10,31 @@ class Flag
   attr_reader :key
   attr_reader :value
 
-  def initialize( *args )
-    fmt = "%08b"
-
-    if args.size == 0
-      @value = 0   ## same as new(0)
-      @key   = :"#{fmt % @value}"   ## use :none for 0 key - why? why not?
-    elsif args.size == 1 && args[0].is_a?(Integer)
-      @value = args[0]
-      @key   = :"#{fmt % @value}"   ## todo: lookup if value is a known flag (with key) - why? why not?
-    elsif args.size == 2 && args[0].is_a?(Symbol) && args[1].is_a?(Integer)
-      @key   = args[0]
-      @value = args[1]
-    else
-      ## assume flag object or symbols
-      @value = 0
-      args.each do |arg|
-        flag = _typecast_flag!( arg )
-        @value |= flag.value
-      end
-      @key = :"#{fmt % @value}"    ## todo: lookup if value is a known flag (with key) - why? why not?
-    end
-    self.freeze  ## make "immutable" - should be a value object like an integer number!!!
-    self # return self for chaining
+  def initialize( key, value )
+    @key   = key
+    @value = value
+    self.freeze
+    self
   end
 
-
-  def _typecheck_flag!( o )
-    if self.class == o.class
+  def self._typecheck_flag!( o )
+    if self == o.class
       o
     else
-      raise TypeError.new( "[Flag] flag >#{self.class.name}< type expected; got >#{o.class.inspect}<" )
+      raise TypeError.new( "[Flag] flag >#{name}< type expected; got >#{o.class.inspect}<" )
     end
   end
 
-  def _typecast_flag!( o )
+  def self._typecast_flag!( o )
     if o.is_a? Symbol   ## auto-convert symbol to flag
-      o = self.class.key( o )  ## lookup symbol in "parent" flags class
+      o = key( o )  ## lookup symbol in "parent" flags class
     end
     _typecheck_flag!( o )
   end
+
+  def _typecheck_flag!( o ) self.class._typecheck_flag!( o ); end
+  def _typecast_flag!( o ) self.class._typecast_flag!( o ); end
+
 
 
   def ==( other )
@@ -69,51 +54,51 @@ class Flag
 
 
   def bitwise_or( other )
-    self.class.new( @value | _typecheck_flag!( other ).value )
+    self.class.build( @value | _typecheck_flag!( other ).value )
   end
-  alias_method :|,    :bitwise_or
+  alias_method :|, :bitwise_or
 
   def bitwise_and( other )
-    self.class.new( @value & _typecheck_flag!( other ).value )
+    self.class.build( @value & _typecheck_flag!( other ).value )
   end
   alias_method :&, :bitwise_and
 
   def bitwise_xor( other )
-    self.class.new( @value ^ _typecheck_flag!( other ).value )
+    self.class.build( @value ^ _typecheck_flag!( other ).value )
   end
-  alias_method :^,      :bitwise_xor
+  alias_method :^, :bitwise_xor
 
   def bitwise_inverse
-     self.class.new( ~@value )
+     self.class.build( ~@value )
   end
   alias_method :~, :bitwise_inverse
 
 
   def set( other )  ## note: typecast also allows symbol e.g (:read_only, etc.)
-    self.class.new( @value | _typecast_flag!( other ).value )
+    self.class.build( @value | _typecast_flag!( other ).value )
   end
   alias_method :flag, :set
 
   def unset( other )
-    self.class.new( @value & ~_typecast_flag!( other ).value )
+    self.class.build( @value & ~_typecast_flag!( other ).value )
   end
   alias_method :unflag, :unset
 
   def toggle( other )
-    self.class.new( @value ^ _typecast_flag!( other ).value )
+    self.class.build( @value ^ _typecast_flag!( other ).value )
   end
-
 
 
 
   def self.keys()
-    # note: does NOT include none - why? why not?
+    # note: does NOT include :none (and :all)
     @keys ||= members.map { |member| member.key }.freeze
   end
 
   def self.key( key )
-    @hash ||= Hash[ keys.zip( members ) ].freeze
-    @hash[key]
+    # note: does NOT include :none (and :all)
+    @hash_by_key ||= Hash[ keys.zip( members ) ].freeze
+    @hash_by_key[key]
   end
 
   def self.[]( key )  ## convenience alias for key
@@ -122,23 +107,45 @@ class Flag
 
 
   def self.values()
-    # note: does NOT include none - why? why not?
+    # note: does NOT include :none (and :all)
     @values ||= members.map { |member| member.value }.freeze
   end
 
+  def self.value( value )
+    ## note: adds :none and :all for value lookup
+    @hash_by_value ||= Hash[ values.zip( members ) ].merge( self::NONE.value => self::NONE,
+                                                            self::ALL.value  => self::ALL ).freeze
+    @hash_by_value[value]
+  end
 
-  def self.zero() @zero ||= new(0); end
+  def self.build( value )
+    o = self.value( value )   # lookup if value is a known flag (with key)
+    o = new( :"#{'%08b' % value}", value )  if o.nil?
+    o
+  end
+
+
+  def self.zero() @zero ||= self::NONE; end
   def zero?() @value == 0; end
 
 
   def self.convert( *args )
-    new( *args )
+    if args.size == 0
+      self::NONE
+    elsif args.size == 1 && args[0].is_a?(Integer)
+      build( args[0] )
+    else
+      ## assume flag object or symbols
+      value = 0
+      args.each do |arg|
+        flag = _typecast_flag!( arg )
+        value |= flag.value
+      end
+      build( value )
+    end
   end
 
-
   ## add size|length too why? why not?
-  ## add value() lookup?
-  ##   not for now - why? combined values are undefined!! what to return??
 
   ## add to_i, to_int - why? why not?
   ## def to_i()   @value; end
